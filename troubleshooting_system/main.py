f7fd01f60685dd244e4e05825a55e948ba821b96
BondarOleg98 <olegb5195@gmail.com>
2020-05-27 00:28:27 +0300

    Start new project

---
+++ troubleshooting_system/main.py
@@ -0,0 +1,18 @@
+import welcome_window as ww
+from tkinter import *
+from PIL import ImageTk, Image
+
+
+def call_window(window):
+    app = ww.WelcomeWindow(window)
+    app.pack()
+
+
+if __name__ == "__main__":
+    root = Tk()
+    call_window(root)
+    canvas = Canvas(root, width=650, height=450)
+    img = ImageTk.PhotoImage(Image.open("background.jpg"))
+    canvas.create_image(0, 0, anchor=NW, image=img)
+    canvas.pack()
+    root.mainloop()
---
+++ troubleshooting_system/welcome_window.py
@@ -0,0 +1,46 @@
+import os
+import tkinter
+import function_window as fw
+from tkinter import filedialog
+from tkinter import *
+import pandas as pd
+
+
+class WelcomeWindow(tkinter.Frame):
+    def __init__(self, root):
+        super().__init__(root)
+        self.icon_img = tkinter.PhotoImage(file="file_icon.gif")
+        self.root = root
+        self.init_main()
+
+    def init_main(self):
+        self.menu_window()
+        self.root.title("Searching app failures")
+        self.root.geometry("650x450+300+200")
+        self.root.resizable(False, False)
+        welcome_label = Label(text="Welcome to system", font="Arial 32")
+
+        choose_frame = LabelFrame(text="Choose file", height=200, width=200, font="Arial 12")
+        btn = tkinter.Button(choose_frame, command=self.open_dialog, image=self.icon_img)
+        btn.pack()
+        welcome_label.pack()
+        choose_frame.pack()
+
+    def open_dialog(self):
+        file = filedialog.askopenfilename(
+            initialdir='/',
+            initialfile='tmp',
+            filetypes=[("CSV", "*.csv")])
+        if file != "":
+            os.startfile(file)
+            fw.FunctionWindow(self.root)
+
+    def menu_window(self):
+        menu = Menu(self.root)
+        self.root.config(menu=menu)
+        file_menu = Menu(menu, tearoff=0)
+        file_menu.add_command(label="Exit", command=self.exit_system)
+        menu.add_cascade(label="File", menu=file_menu)
+
+    def exit_system(self):
+        self.root.destroy()
